1afae02a8134fb4443bc0b7698c4abbc14bcf94f
Avinash <avinashkr30122002@gmail.com>
2025-02-24 14:40:42 +0530

    update

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -1,6 +1,8 @@
-export default ExampleComponent = () => (
+const ExampleComponent = () => (
     <div>
       <h1>Test Component</h1>
       <button>Click me</button>
     </div>
   );
+
+export default ExampleComponent;
