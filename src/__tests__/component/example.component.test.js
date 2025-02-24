c3c473edf8783c947dec5b5828662bf7b899bc8f
Avinash <avinashkr30122002@gmail.com>
2025-02-24 14:37:09 +0530

    update

--- src/__tests__/component/example.component.test.js
+++ src/__tests__/component/example.component.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+// import '@testing-library/jest-dom/extend-expect';
 import ExampleComponent from '../../components/ExampleComponent';
 
 // Component test example
