dcee3ce5f2cabc7928780bbafd152a23da367457
Avinash <avinashkr30122002@gmail.com>
2025-02-25 14:50:33 +0530

    update

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,10 @@ module.exports = {
       '/node_modules/',
       '/build/'
     ],
+    moduleNameMapper: {
+      '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/src/__mocks__/fileMock.js',
+      "\\.(css|less|scss|sass)$": "<rootDir>/src/__mocks__/styleMock.js"
+    },
     collectCoverageFrom: [
       'src/**/*.{js,jsx}',
       '!src/index.jsx',
