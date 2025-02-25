2cd64bcdb5f9440da8dab51cf470fb33e1264916
Avinash <avinashkr30122002@gmail.com>
2025-02-25 12:15:15 +0530

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {files: ["**/*.{js,mjs,cjs,jsx}"], ignores: ["node_modules", "jest.config.js", "vite.config.js", "jest.setup.js", "babel.config.js"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
